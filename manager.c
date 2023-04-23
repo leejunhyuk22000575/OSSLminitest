18f392f47c33c41d67f86f54f470beece6385224
22000575LeeJunhyuk <dlwnsgur1552gmail.com>
2023-04-23 18:14:45 +0900

    Add LoadData

--- manager.c
+++ manager.c
@@ -48,17 +48,23 @@ void saveData(Product p[], int count){
     fclose(fp);
     printf("저장됨!\n");
 }
-
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
+    int count=0;
+    FILE*fp;
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
+    fp= fopen("product.txt","rt");
 
+    if(fp == NULL){
+        printf("=> 파일 없음\n");
+        return 0;
+    }
 
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+    while(!feof(fp)){
+        fscanf(fp, "%d %d %[^\n]\n", &p[count].weight, &p[count].price, p[count].name);
+        count++;
+    }
+ fclose(fp);
+    printf("=> 로딩 성공!\n");
+    return count;
 }
